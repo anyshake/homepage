22b77b3f6a0ed00198856d39cc68840077878552
Chengxun Lee <24319042+bclswl0827@users.noreply.github.com>
2023-12-13 00:54:21 +0800

    Make anyshake.org work

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className="w-full min-h-screen flex">
+            <h1 className="p-5 m-auto text-center font-bold text-2xl">
+                We're working on it!
+            </h1>
+        </div>
+    );
+};
 
 export default App;
