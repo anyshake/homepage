7b5e70458f5b9cb6db51ee897c67f1283ede19fc
Chengxun Lee <24319042+bclswl0827@users.noreply.github.com>
2023-12-17 00:01:39 +0800

    Update feature items

--- src/config/features.ts
+++ src/config/features.ts
@@ -1,32 +1,32 @@
 export const featuresConfig = [
     {
-        title: "Better Solution",
+        title: "Superior Solution",
         description:
-            "AnyShake is a better solution for the most current homemade seismograph that based on MPU6050, providing more probablity to catch the earthquakes.",
+            "AnyShake offers a superior alternative to the most current homemade seismographs based on the MPU6050, increasing the likelihood of detecting earthquakes.",
     },
     {
-        title: "Cross Platform",
+        title: "Cross-Platform",
         description:
-            "AnyShake Observer is a cross-platform application written in Go, it can work perfectly on Windows, macOS, Linux, or even embedded devices.",
+            "The AnyShake Observer is a cross-platform application written in Go, ensuring seamless functionality on Windows, macOS, Linux, and even embedded devices.",
     },
     {
-        title: "Standards Compliant",
+        title: "Standards Compliance",
         description:
-            "AnyShake Observer has ability to export seismic data in SAC or MiniSEED format, which can be used by most of the seismic data processing software.",
+            "AnyShake Observer supports exporting seismic data in SAC or MiniSEED formats, compatible with most seismic data processing software.",
     },
     {
         title: "Open Source",
         description:
-            "AnyShake is an open-source project, the source code is available on GitHub, anyone can build their own seismograph with the help of AnyShake.",
+            "AnyShake is an open-source project with its source code available on GitHub, enabling anyone to build their own seismograph using AnyShake.",
     },
     {
-        title: "Totally Free",
+        title: "Completely Free",
         description:
-            "AnyShake Explorer & AnyShake Observer are licensed under the MIT license, which means you can use them for any purpose, even for commercial projects.",
+            "AnyShake Explorer & AnyShake Observer are released under the MIT license, granting free use for any purpose, including commercial projects.",
     },
     {
-        title: "Pravicy Matters",
+        title: "Privacy Matters",
         description:
-            "AnyShake Explorer & AnyShake Observer never collect any personal information or data from users and send them to remote servers, your pravicy really matters.",
+            "AnyShake Explorer & AnyShake Observer do not collect or transmit any personal information from users, ensuring your privacy is always respected.",
     },
 ];
